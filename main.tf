235aa248f7517da6fcac2824cc5a8d7f3e1b4aec
Amrita-N <93979400+Amrita-N@users.noreply.github.com>
2024-03-05 11:38:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
  
 resource "aws_instance" "yourec2" {
-  ami                    = "ami-0e670eb768a5fc3d4 "
+  ami                    = "ami-0e670eb768a5fc3d4"
   instance_type          = "t2.micro"
   tags = {
     Name = "terraform-inst"
